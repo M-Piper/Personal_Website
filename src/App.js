097c12722c75169b197376d7d3be609ecdee9ff7
M-Piper <margaret.f.piper@gmail.com>
2024-03-06 09:10:16 -0500

    main page working as app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React from 'react';
+//testing new imports
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import CV from './components/CV';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+//end of testing imports
 function App() {
+  return (
+      <div className="App">
+          <header className="App-header">
+      <h1>Margaret Piper</h1>
+      <Router>
+        <Routes>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/cv" component={CV} />
+          <Route path="/projects" component={Projects} />
+          <Route path="/contact" component={Contact} />
+        </Routes>
+      </Router>
+          </header>
+      </div>
+  );
+}
+
+export default App;
+  /*
   return (
     <div className="App">
       <header className="App-header">
@@ -21,5 +49,4 @@ function App() {
     </div>
   );
 }
-
-export default App;
+export default App;*/
