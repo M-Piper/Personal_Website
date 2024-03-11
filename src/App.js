ebd172b0658a6deaa610a23bf1db830a92d40951
M-Piper <margaret.f.piper@gmail.com>
2024-03-11 00:23:41 -0400

    All four working - minor adjustments needed to two hover images

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import React, { useRef, useState } from 'react';
 import './App.css';
 import mainImage from './buttons/no_selection.png';
 import projectsHoverImage from './buttons/projects.png';
+import aboutHoverImage from './buttons/about.png';
+import cvHoverImage from './buttons/cv.png';
+import contactHoverImage from './buttons/contact.png';
 
 function App() {
     const [hovering, setHovering] = useState(false);
+    const [hoverImage, setHoverImage] = useState(null);
     const containerRef = useRef(null);
 
     const handleMouseEnter = () => {
@@ -18,18 +22,29 @@ function App() {
     const handleMouseMove = (event) => {
         const { offsetX, offsetY } = event.nativeEvent;
 
-        // Check if the mouse is within the polygon defined by the coordinates
-        if (isPointInPoly(offsetX, offsetY)) {
-            setHovering(true);
+        // Check if the mouse is within any of the polygons
+        const { project, about, contact, cv } = isPointInPolygons(offsetX, offsetY);
+
+        // Display the corresponding hover image based on which polygon is being hovered over
+        if (project) {
+            setHoverImage(projectsHoverImage);
+        } else if (about) {
+            setHoverImage(aboutHoverImage);
+        } else if (contact) {
+            setHoverImage(contactHoverImage);
+        } else if (cv) {
+            setHoverImage(cvHoverImage);
         } else {
-            setHovering(false);
+            setHoverImage(null);
         }
+
+        // Set hovering state to true if hovering over any polygon
+        setHovering(project || about || contact || cv);
     };
 
-    // Function to check if a point is within a polygon
-    const isPointInPoly = (x, y) => {
-        // Define the polygon coordinates
-        const polyCoords = [
+    const isPointInPolygons = (x, y) => {
+        // Define the polygon coordinates for each button
+        const projectPoly = [
             [582, 264],
             [549, 319],
             [810, 441],
@@ -43,14 +58,66 @@ function App() {
             [661, 228]
         ];
 
+        const aboutPoly = [
+            [545,543],
+            [526,477],
+            [524,396],
+            [549,318],
+            [814,442],
+            [663,693],
+            [616,647],
+            [570,597]
+        ];
+
+        const contactPoly = [
+            [1109, 451],
+            [1418, 299],
+            [1446, 375],
+            [1451, 452],
+            [1420, 566],
+            [1352, 649],
+            [1296, 698],
+            [1197, 755]
+        ];
+
+        const cvPoly = [
+            [963, 361],
+            [936, 65],
+            [1006, 67],
+            [1044, 76],
+            [1111, 87],
+            [1164, 99],
+            [1221, 120],
+            [1272, 152],
+            [1345, 200],
+            [1387, 253],
+            [1415, 299],
+            [1113, 447]
+        ];
+
+        // Check if the point is within any of the polygons
+        const isInsideProject = isPointInPolygon(projectPoly, x, y);
+        const isInsideAbout = isPointInPolygon(aboutPoly, x, y);
+        const isInsideContact = isPointInPolygon(contactPoly, x, y);
+        const isInsideCV = isPointInPolygon(cvPoly, x, y);
+
+        return {
+            project: isInsideProject,
+            about: isInsideAbout,
+            contact: isInsideContact,
+            cv: isInsideCV
+        };
+    };
+
+    const isPointInPolygon = (polygon, x, y) => {
         let isInside = false;
 
         // Ray casting algorithm to check if the point is inside the polygon
-        for (let i = 0, j = polyCoords.length - 1; i < polyCoords.length; j = i++) {
-            const xi = polyCoords[i][0];
-            const yi = polyCoords[i][1];
-            const xj = polyCoords[j][0];
-            const yj = polyCoords[j][1];
+        for (let i = 0, j = polygon.length - 1; i < polygon.length; j = i++) {
+            const xi = polygon[i][0];
+            const yi = polygon[i][1];
+            const xj = polygon[j][0];
+            const yj = polygon[j][1];
 
             const intersect =
                 yi > y !== yj > y && x < ((xj - xi) * (y - yi)) / (yj - yi) + xi;
@@ -71,8 +138,7 @@ function App() {
                 onMouseMove={handleMouseMove}
             >
                 <img src={mainImage} alt="main-image" className="main-image" />
-
-                {hovering && <img src={projectsHoverImage} alt="projects-hover-image" className="projects-hover-image" />}
+                {hovering && hoverImage && <img src={hoverImage} alt="hover-image" className="hover-image" />}
             </div>
         </div>
     );
