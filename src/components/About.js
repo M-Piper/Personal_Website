aa19d6b02ccb98181e3d9d3ab7b060b93f08dd7f
M-Piper <margaret.f.piper@gmail.com>
2024-03-15 17:57:39 -0400

    content revised. scroll needs fixing still

--- src/components/About.js
+++ src/components/About.js
@@ -14,12 +14,12 @@ const About = () => {
         <div className="about-bg">
             <div className="about-content">
                 <div id="aboutMargaret">
-                    <p>While new to working as a developer, Margaret is no stranger to the Canadian workforce. From her diverse background as a professional musician, librarian, archivist, and now IT professional, her creativity shines through all of her work experiences and she is known for her strong work ethic, creativity, and ability to learn on the fly.
-
-                        Margaret enjoyed a lengthy career in the field of librarianship and found herself in increasingly technical roles, culminating in a Systems Librarian position. At that point, she decided to make the leap and return to school to learn coding and become a full-stack developer. She was fortunate to be able to work full time for the Federal Government of Canada in an IT capacity during her studies.
+                    <p>While new to working as a developer, Margaret is no stranger to the Canadian workforce. From her diverse background as a professional musician, librarian, archivist, and now IT professional, her creativity shines through all of her work experiences. She is known for her strong work ethic, creativity, and ability to learn on the fly.
+                    </p>
+                    <p>
+                        Margaret enjoyed a lengthy career in the field of librarianship and found herself in increasingly technical roles, culminating in a Systems Librarian position. At that point, she decided to make the leap and return to school to learn coding and become a full-stack developer. She was fortunate to be able to work full-time for the Federal Government of Canada in an IT capacity during her studies. And has chosen to pursue work in the private sector.
 
                         In her spare time, Margaret enjoys sewing, cooking and gardening. She lives in the countryside between Ottawa and Montreal with her husband and three cats.
-                        [draft]
                     </p>
                 </div>
             </div>
--- src/components/CV.js
+++ src/components/CV.js
@@ -38,31 +38,33 @@ const CV = () => {
                         )}
                         {activeSection === 1 && (
                             <p>
-                                Junior Software Analyst, Department of National Defence (March 2023 – present) <br />
-                                Systems Librarian, SDG Library (August 2020 – October 2021) <br />
+                                Jr Software Analyst, Department of National Defence (March - Dec. 2023) <br />
+                                Systems Librarian, SDG Library (Aug 2020 – Oct 2021) <br />
                                 Library and Archives Canada (2016-2020) <br />
-                                Archivist, Governance Portfolio, Private Archives (January 2020-August 2020) <br />
-                                Project Librarian, Library and Archives Canada (November 2016 – December 2019) <br />
-                                Archival Assistant, Library and Archives Canada (September 2015 – November 2016) <br />
-                                Information Management Officer, Canada Revenue Agency (May-August 2015) <br />
+                                Archivist, Governance Portfolio, Private Archives <br />
+                                Project Librarian, Library and Archives Canada <br />
+                                Archival Assistant, Library and Archives Canada<br />
+                                Information Management Officer, Canada Revenue Agency (2015) <br />
                                 Library Assistant, Canadiana.org (2014)
                             </p>
                         )}
                         {activeSection === 2 && (
                             <p>
-                                Developing with AWS (2023) <br />
-                                Management Training (2020) <br />
-                                Transforming and Querying XML with XSLT and XQuery (2020) <br />
-                                Resource Description and Access (Library Cataloguing) (2018) <br />
+                                <br/>
+                                <br/>
+                                Developing with AWS Workshop, 2023 <br />
+                                Management Training Workshop, 2020 <br />
+                                Transforming and Querying XML with XSLT and XQuery Course, 2020 <br />
+                                Resource Description and Access (Library Cataloguing) Course, 2018 <br />
                                 Board Member, Canadian Association of Music Libraries, 2018-2020 <br />
                                 Conference co-presenter: Ontario Association of Library Technicians, 2021 <br />
                                 Conference Co-presenter at Association of Canadian Archivists, 2016 <br />
-                                Conference Presenter (on behalf of LAC), Canadian Association of Music Libraries, 2018 <br />
-                                Library and Archives Canada Blog - various publications: <br />
-                                <a href="https://thediscoverblog.com/2018/02/06/new-princes-toronto-band/">New Prince Toronto Band, </a>
-                                <a href="https://thediscoverblog.com/2018/02/13/new-additions-to-the-virtual-gramophone-band-and-instrumental-music/"> Instrumental Music, </a>
-                                <a href="https://thediscoverblog.com/2018/01/04/new-additions-to-the-virtual-gramophone-romeo-beaudry/">Roméo Beaudry, </a>
-                                <a href="https://thediscoverblog.com/2017/11/17/new-additions-to-the-virtual-gramophone-henry-burr/">and Henry Burr</a> <br />
+                                Conference Presenter, Canadian Association of Music Libraries, 2018 <br />
+                                <h3>Publications: </h3>
+                                <a href="https://thediscoverblog.com/2018/02/06/new-princes-toronto-band/">New Prince Toronto Band </a> (LAC blog post)<br />
+                                <a href="https://thediscoverblog.com/2018/02/13/new-additions-to-the-virtual-gramophone-band-and-instrumental-music/"> Instrumental Music </a>(LAC blog post)<br />
+                                <a href="https://thediscoverblog.com/2018/01/04/new-additions-to-the-virtual-gramophone-romeo-beaudry/">Roméo Beaudry </a>(LAC blog post)<br />
+                                <a href="https://thediscoverblog.com/2017/11/17/new-additions-to-the-virtual-gramophone-henry-burr/">Henry Burr</a>(LAC blog post)<br />
                                 <a href="https://cmccanada.org/shop/76656/">Small Rooms for Solo Harp (sheet music publication)</a> <br />
                                 <a href="https://music.apple.com/ca/album/canadian-music-for-harp/597495986">Canadian Music for Harp, Lori Gemmell (audio recording)</a>
                             </p>
