06fd4b97e24541b3aea63fadda4a3b2913f04ff2
M-Piper <margaret.f.piper@gmail.com>
2024-03-27 15:25:47 -0400

    Added About carousel. Corrected arrow colours so they are easier to see.

--- src/components/About.js
+++ src/components/About.js
@@ -13,7 +13,7 @@ const About = () => {
                 <div className="about-carousel-slide">
                     <img src={about} alt="About" className="about-carousel-image"/>
                     <div className="about-carousel-content">
-                        <p className="slide-text">
+                        <p className="about-slide-text">
                         While new to working as a developer, Margaret is no stranger to the Canadian workforce.
                         From her diverse background as a professional musician, librarian, archivist, and now IT professional,
                         her creativity shines through all of her work experiences. She is known for her strong work ethic,
@@ -24,7 +24,7 @@ const About = () => {
                 <div className="about-carousel-slide">
                     <img src={about} alt="About" className="about-carousel-image"/>
                     <div className="about-carousel-content">
-                    <p className="slide-text">
+                    <p className="about-slide-text">
                         Margaret enjoyed a lengthy career in the field of librarianship and found herself in increasingly technical roles,
                         culminating in a Systems Librarian position. At that point, she decided to make the leap and return to school to learn coding
                         and become a full-stack developer. She was fortunate to be able to work full-time for the Federal Government of Canada
@@ -35,7 +35,7 @@ const About = () => {
                 <div className="about-carousel-slide">
                     <img src={about} alt="About" className="about-carousel-image"/>
                     <div className="about-carousel-content">
-                    <p className="slide-text">
+                    <p className="about-slide-text">
                         In her spare time, Margaret enjoys sewing, cooking, and gardening. She lives in the countryside between
                         Ottawa and Montreal with her husband and three cats.
                     </p>
