d13dfe4c4aec2d5c8bce3f947d02bf23ce8613c9
M-Piper <margaret.f.piper@gmail.com>
2024-05-31 13:52:57 -0400

    corrected about carousel

--- src/components/About.js
+++ src/components/About.js
@@ -10,33 +10,37 @@ const About = () => {
         <div className="about-bg">
             <Sidebar/>
             <Carousel showArrows={true} showThumbs={false} showStatus={false} className="about-carousel">
-                <div className="about-carousel-slide">
+                <div className="carousel-slide">
                     <img src={about} alt="About" className="about-carousel-image"/>
-                    <div className="about-carousel-content">
+                    <div className="carousel-content">
                         <p className="about-slide-text">
                         While new to working as a developer, Margaret is no stranger to the Canadian workforce.
                         From her diverse background as a professional musician, librarian, archivist, and now IT professional,
                         her creativity shines through all of her work experiences. She is known for her strong work ethic,
-                        creativity, and ability to learn on the fly.<br/>
-                    </p>
+                            creativity, and ability to learn on the fly.
+                        </p>
+                 {/*   </p>
                 </div>
                 </div>
-                <div className="about-carousel-slide">
+                <div className="carousel-slide">
                     <img src={about} alt="About" className="about-carousel-image"/>
-                    <div className="about-carousel-content">
-                    <p className="about-slide-text">
+                    <div className="carousel-content">
+                    <p className="slide-text">*/}
+                        <p className="about-slide-text">
                         Margaret enjoyed a lengthy career in the field of librarianship and found herself in increasingly technical roles,
                         culminating in a Systems Librarian position. At that point, she decided to make the leap and return to school to learn coding
                         and become a full-stack developer. She was fortunate to be able to work full-time for the Federal Government of Canada
-                        in an IT capacity during her studies and has now chosen to pursue work in the private sector.
-                    </p>
+                            in an IT capacity during her studies and has now chosen to pursue work in the private sector.
+                        </p>
+                 {/*   </p>
                 </div>
                 </div>
-                <div className="about-carousel-slide">
+                <div className="carousel-slide">
                     <img src={about} alt="About" className="about-carousel-image"/>
-                    <div className="about-carousel-content">
-                    <p className="about-slide-text">
-                        In her spare time, Margaret enjoys sewing, cooking, and gardening. She lives in the countryside between
+                    <div className="carousel-content">
+                    <p className="slide-text">*/}
+                        <p className="about-slide-text">
+                            In her spare time, Margaret enjoys sewing, cooking, and gardening. She lives in the countryside between
                         Ottawa and Montreal with her husband and three cats.
                     </p>
                 </div>
