243e4d116913e61ce97e6592c9713125c03fc630
M-Piper <margaret.f.piper@gmail.com>
2024-06-09 15:36:21 -0400

    added canfly link

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,6 +9,8 @@ import contactHoverImage from '../buttons/contact.png';
 import homeHoverImage from '../buttons/home.png';
 import Sidebar from './Sidebar';
 import monster from '../images/WynneTheUncertain.png';
+import canfly from '../images/'canfly.png';
+
 const Projects = () => {
     return (
         <div className="projects-bg">
@@ -22,7 +24,13 @@ const Projects = () => {
                     <img src={monster} alt="worriedMonsters" className="worried-monsters-image" />
                 </div>
             </Link>
-        </div>
+            <Link to="https://ashy-desert-0cf93a21e.5.azurestaticapps.net/" className="worriedMonsters-link">
+        <div className = "canfly">
+            <div className = "text-container">
+                <h3> Pilot Logbook</h3>
+            </div>
+            <img src={canfly} alt="canfly" className="canfly-image"
+            </div>
     );
 };
 
